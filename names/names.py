f27e92abd710c99f1a3eebb58694c78852c356f2
kenna <kennaalastair@gmail.com>
2020-02-28 10:25:15 -0800

    names.py complete

--- names/names.py
+++ names/names.py
@@ -13,14 +13,30 @@ f.close()
 duplicates = []  # Return the list of duplicates in this data structure
 
 # Replace the nested for loops below with your improvements
-for name_1 in names_1:
-    for name_2 in names_2:
-        if name_1 == name_2:
-            duplicates.append(name_1)
+# Lambda starter code to compare
+# for name_1 in names_1:
+#     for name_2 in names_2:
+#         if name_1 == name_2:
+#             duplicates.append(name_1)
+
+# runtime: 7.265299320220947 seconds
+
+#------------------------------------- My solution --------------------------------------------
+
+# My solution
+# Enumerate() method adds a counter to an iterable and returns it in a form of enumerate object.
+# This enumerate object can then be used directly in for loops or be converted into a list of tuples
+# using list() method.Enumerate() method adds a counter to an iterable and returns it in a form of enumerate object.
+# This enumerate object can then be used directly in for loops or be converted into a list of tuples using list() method.
+for name in enumerate(names_1):
+    if name[1] in names_2:
+        duplicates.append(name[1])
 
 end_time = time.time()
 print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
 print (f"runtime: {end_time - start_time} seconds")
+
+# runtime: 1.1183538436889648 seconds
 
 # ---------- Stretch Goal -----------
 # Python has built-in tools that allow for a very efficient approach to this problem
